d8a486b413bdc5594a80124a3fbddafc89939594
AryaBhole <bholeaarya@gmail.com>
2025-12-22 13:33:58 +0530

    Revising old concepts

---
+++ Relearn.java
@@ -0,0 +1,8 @@
+public class Relearn {
+    public static void main(String[] args) {
+        String a = "Arya";
+        String  b = "B";
+        String name = a + " " + b;
+        System.out.println("Hello " + name);
+    }
+}
