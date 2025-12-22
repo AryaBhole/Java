35d2e32137880e005da9be461c3bdfd9232ac098
AryaBhole <bholeaarya@gmail.com>
2025-12-22 14:49:03 +0530

    Revising old concepts 2

--- Relearn.java
+++ Relearn.java
@@ -1,8 +1,13 @@
 public class Relearn {
     public static void main(String[] args) {
-        String a = "Arya";
-        String  b = "B";
-        String name = a + " " + b;
-        System.out.println("Hello " + name);
+        // String a = "Arya";
+        // String  b = "B";
+        // String name = a + " " + b;
+        // System.out.println("Hello " + name);
+
+        //area of rectangle
+        int l = 4;
+        int b = 5;
+        System.out.println("The area of the rectangle is : " + (l * b) + "m2");
     }
 }
