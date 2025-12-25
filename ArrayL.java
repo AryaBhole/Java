7ff739c715271e31509f5aab701982cb91262832
AryaBhole <bholeaarya@gmail.com>
2025-12-25 14:47:45 +0530

    Array List learn

---
+++ ArrayL.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayL{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //list.add();
+        //lsit.remove(index);
+        //list.set(index, thing);
+        //list.get(index);
+        //list.size() //returns total size of the array list(number of objects in it)
+        //seems similar to lists in python
+        //Collections.sort(list); //sorts in alphabetical order (This will import the
+        //collections util on top)
+
+        //can use scanner and other stuff as well to add to this list
+    }
+}
