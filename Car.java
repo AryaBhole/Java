2cd6a9d27f11e2d99f54884c1273c44b5329335e
AryaBhole <bholeaarya@gmail.com>
2025-12-25 13:36:09 +0530

    ToString working for direct call

--- Car.java
+++ Car.java
@@ -8,4 +8,8 @@ public class Car {
     void drive(){
         System.out.println("We are driving a " + this.colour + " car");
     }
+    @Override
+    public String toString(){
+        return "Banana " + this.colour;
+    }
 }
---
+++ Tostring.java
@@ -0,0 +1,7 @@
+public class Tostring{
+    public static void main(String[] args) {
+        Car car = new Car("Yellow");
+
+        System.out.println(car);
+    }
+}
