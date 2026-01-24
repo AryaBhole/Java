02413fed6076b74fc1dc31cb4d445c71cb7a6423
AryaBhole <bholeaarya@gmail.com>
2026-01-25 03:29:42 +0530

    NPTEL Week 2 stuff part 1

---
+++ NPTEL/W02_P1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class W02_P1 {
+    public static void main(String[] args) {
+
+        // Create Scanner object to read input from keyboard
+        Scanner sc = new Scanner(System.in);
+
+        // Read the length of the rectangle
+        int length = sc.nextInt();
+
+        // Read the width of the rectangle
+        int width = sc.nextInt();
+
+        int area = length * width;
+
+        // Print the calculated area
+        System.out.println("Area is: " + area);
+
+        // Close the scanner
+        sc.close();
+    }
+}
---
+++ NPTEL/W02_P4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class W02_P4 {
+
+    static class Rectangle {
+        int length;
+        int width;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int l = sc.nextInt();
+        int w = sc.nextInt();
+
+        Rectangle rect = new Rectangle();
+        rect.length = l;
+        rect.width = w;
+
+        
+
+        sc.close();
+    }
+}
