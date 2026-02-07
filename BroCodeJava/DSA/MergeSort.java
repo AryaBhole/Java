afdd3366195a773d51f73aded3ce96df0d9a365e
AryaBhole <bholeaarya@gmail.com>
2026-02-08 03:55:44 +0530

    Better merge sort from striver

--- BroCodeJava/DSA/MergeSort.java
+++ BroCodeJava/DSA/MergeSort.java
@@ -1,84 +1,59 @@
-import java.util.Random;
+import java.util.*;
+
+class Solution {
+    // Function to merge two halves
+    public void merge(int[] arr, int low, int mid, int high) {
+        // Create temp array
+        List<Integer> temp = new ArrayList<>();
+        int left = low, right = mid + 1;
+
+        // Merge both sorted parts
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.add(arr[left++]);
+            else
+                temp.add(arr[right++]);
+        }
+
+        // Add remaining left elements
+        while (left <= mid)
+            temp.add(arr[left++]);
+
+        // Add remaining right elements
+        while (right <= high)
+            temp.add(arr[right++]);
+
+        // Copy back to original array
+        for (int i = low; i <= high; i++)
+            arr[i] = temp.get(i - low);
+    }
+
+    // Recursive merge sort
+    public void mergeSort(int[] arr, int low, int high) {
+        if (low >= high)
+            return;
+
+        // Find mid index
+        int mid = (low + high) / 2;
+
+        // Sort left half
+        mergeSort(arr, low, mid);
+
+        // Sort right half
+        mergeSort(arr, mid + 1, high);
+
+        // Merge both halves
+        merge(arr, low, mid, high);
+    }
+}
 
 public class MergeSort {
-  public static void main(String[] args) {
-
-    Random rand = new Random();
-    int[] numbers = new int[10];
-
-    for (int i = 0; i < numbers.length; i++) {
-      numbers[i] = rand.nextInt(1000000);
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 4, 1};
+        Solution sol = new Solution();
+        sol.mergeSort(arr, 0, arr.length - 1);
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
     }
-
-    System.out.println("Before:");
-    printArray(numbers);
-
-    mergeSort(numbers); 
-
-    System.out.println("\nAfter:");
-    printArray(numbers);
-  }
-
-
-  private static void mergeSort(int[] inputArray) {
-    int inputLength = inputArray.length;
-    
-    if (inputLength < 2) {
-      return;
-    }
-    
-    int midIndex = inputLength / 2;
-    int[] leftHalf = new int[midIndex];
-    int[] rightHalf = new int[inputLength - midIndex];
-    
-    for (int i = 0; i < midIndex; i++) {
-      leftHalf[i] = inputArray[i];
-    }
-    for (int i = midIndex; i < inputLength; i++) {
-      rightHalf[i - midIndex] = inputArray[i];
-    }
-    
-    mergeSort(leftHalf);
-    mergeSort(rightHalf);
-    
-    merge(inputArray, leftHalf, rightHalf);
-  }
-
-  private static void merge (int[] inputArray, int[] leftHalf, int[] rightHalf) {
-    int leftSize = leftHalf.length;
-    int rightSize = rightHalf.length;
-    
-    int i = 0, j = 0, k = 0;
-    
-    while (i < leftSize && j < rightSize) {
-      if (leftHalf[i] <= rightHalf[j]) {
-        inputArray[k] = leftHalf[i];
-        i++;
-      }
-      else {
-        inputArray[k] = rightHalf[j];
-        j++;
-      }
-      k++;
-    }
-    
-    while (i < leftSize) {
-      inputArray[k] = leftHalf[i];
-      i++;
-      k++;
-    }
-    
-    while (j < rightSize) {
-      inputArray[k] = rightHalf[j];
-      j++;
-      k++;
-    }
-    
-  }
-
-  private static void printArray(int[] numbers) {
-    for (int i = 0; i < numbers.length; i++) {
-      System.out.println(numbers[i]);
-    }
-  }
 }
