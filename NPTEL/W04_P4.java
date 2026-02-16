fd6fb053398882dc7baeea0fa1817aaec3781e5d
AryaBhole <bholeaarya@gmail.com>
2026-02-17 00:08:56 +0530

    Week 4 NPTEL Programming Assignment Complete

---
+++ NPTEL/W04_P4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class W04_P4 {
+
+    static class Employee {
+
+        // Protected variable
+        protected int salary;
+    }
+
+    static class Manager extends Employee {
+        // Inherits salary
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int s = sc.nextInt();
+
+        Manager m = new Manager();
+
+        m.salary = s;
+		System.out.print("Salary is: " + m.salary);
+
+        sc.close();
+    }
+}
