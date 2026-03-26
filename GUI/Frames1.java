db41eefc5409f809eeaab8870d319c91a9534a94
AryaBhole <bholeaarya@gmail.com>
2026-03-27 02:14:30 +0530

    made frame using constructor in a seperate parent class MyFrame
    made a constructor for jFrame outside the file revised old concepts

--- GUI/Frames1.java
+++ GUI/Frames1.java
@@ -1,22 +1,20 @@
-import java.awt.Color;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 
 public class Frames1 {
     public static void main(String[] args) {
         
-        JFrame frame = new JFrame();
-        frame.setTitle("Title goes here");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(480, 480);
-        frame.setVisible(true);
+        // JFrame frame = new JFrame();
+        // frame.setTitle("Title goes here");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setResizable(false);
+        // frame.setSize(480, 480);
+        // frame.setVisible(true);
 
-        ImageIcon icon = new ImageIcon("logo.png");
-        frame.setIconImage(icon.getImage());
+        // ImageIcon icon = new ImageIcon("logo.png");
+        // frame.setIconImage(icon.getImage());
 
-        frame.getContentPane().setBackground(Color.CYAN); //can use rgb values with new Color(0, 0, 0) or hex code with new Color(0x000FFF)
+        // frame.getContentPane().setBackground(Color.CYAN); //can use rgb values with new Color(0, 0, 0) or hex code with new Color(0x000FFF)
 
-        
+        MyFrame myFrame = new MyFrame();
+        //still creates the same output and you can also just use //new MyFrame(); but it wont be editable
     }
 }
