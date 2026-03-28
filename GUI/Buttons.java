edd919ff4620dcd3d3e0562879c2ffe5760a1f6b
AryaBhole <bholeaarya@gmail.com>
2026-03-28 22:53:42 +0530

    Modified the My frame for buttons module and did some action listener stuff

---
+++ GUI/Buttons.java
@@ -0,0 +1,7 @@
+public class Buttons {
+    public static void main(String[] args) {
+        new MyFrame();
+
+        
+    }
+}
--- GUI/MyFrame.java
+++ GUI/MyFrame.java
@@ -1,21 +1,34 @@
 // Instead of using JFrame directly in the file you can use a parent class as such
-import java.awt.Color;
-import javax.swing.ImageIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener{
     //Make a constructor with the same code from the file but this saves time reusability and changes can be made with the original file itself
+    
+    JButton button;
+    
     MyFrame() {
+        
+        button = new JButton();
+        button.setBounds(200, 100, 100, 5);
+        button.addActionListener(this);
 
-        this.setTitle("Title goes here");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setSize(480, 480);
+        this.setSize(500, 500);
         this.setVisible(true);
+        this.add(button);
+    }
 
-        ImageIcon icon = new ImageIcon("logo.png");
-        this.setIconImage(icon.getImage());
-
-        this.getContentPane().setBackground(Color.CYAN);
-    }    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("hii");
+        }
+    }   
 }
+
+// we can remove the actionListener tihing entirely and do button.addActionListener(e -> System.out.println());
+// this is a lambda expression
