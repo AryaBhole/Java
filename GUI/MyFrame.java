f3e17bc33ef2b1dbd9a48fbc9c208bf51fc47afa
AryaBhole <bholeaarya@gmail.com>
2026-03-31 04:07:20 +0530

    Text Fields in java

--- GUI/MyFrame.java
+++ GUI/MyFrame.java
@@ -1,42 +1,43 @@
 // Instead of using JFrame directly in the file you can use a parent class as such
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class MyFrame extends JFrame implements ActionListener{
     //Make a constructor with the same code from the file but this saves time reusability and changes can be made with the original file itself
     
     JButton button;
-    
-    MyFrame() {
-        
-        button = new JButton();
-        button.setBounds(100, 100, 250, 100);
-        button.addActionListener(this);
-        button.setText("Button");
-        button.setFocusable(false);
-        //can add icon same as beafore
-        button.setBorder(BorderFactory.createEtchedBorder());
-        button.setEnabled(false);
-        // you can also make suff appear after 1 click by using actionPerformed thing
+    JTextField textField;
 
+    MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setLayout(null);
-        this.setSize(500, 500);
-        this.setVisible(true);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(250, 40));
+        // can change the font, foreground, background, bliner line(caret) color, default text, setEditable, change this and set enabled in action listner to only take one input        
+
         this.add(button);
+        this.add(textField);
+        this.pack();
+        this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button) {
-            System.out.println("hii");
+        if(e.getSource() == button) {
+            System.out.println("Welcome " + textField.getText());
         }
     }
-}
+}   
 
 // we can remove the actionListener tihing entirely and do button.addActionListener(e -> System.out.println());
 // this is a lambda expression
---
+++ GUI/TextField.java
@@ -0,0 +1,5 @@
+public class TextField {
+    public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
+    }
+}
