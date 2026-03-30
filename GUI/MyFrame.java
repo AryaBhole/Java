e396a5b2ea3107091d431b64ffbb39a815f726e9
AryaBhole <bholeaarya@gmail.com>
2026-03-31 04:37:10 +0530

    Checkbox in swing

--- GUI/MyFrame.java
+++ GUI/MyFrame.java
@@ -1,41 +1,31 @@
 // Instead of using JFrame directly in the file you can use a parent class as such
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 
 public class MyFrame extends JFrame implements ActionListener{
     //Make a constructor with the same code from the file but this saves time reusability and changes can be made with the original file itself
     
-    JButton button;
-    JTextField textField;
 
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setLayout(new FlowLayout());
 
-        button = new JButton("Submit");
-        button.addActionListener(this);
+        JCheckBox checkBox = new JCheckBox();    
+        checkBox.setText("I'm not a robot"); //or pass this directly
+        // can do same stuff as we have been doing uptill now with action listener and other font and icon stuff
 
-        textField = new JTextField();
-        textField.setPreferredSize(new Dimension(250, 40));
-        // can change the font, foreground, background, bliner line(caret) color, default text, setEditable, change this and set enabled in action listner to only take one input        
-
-        this.add(button);
-        this.add(textField);
+        this.add(checkBox);
         this.pack();
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == button) {
-            System.out.println("Welcome " + textField.getText());
-        }
+        
     }
 }   
 
