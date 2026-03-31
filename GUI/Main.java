9b841c13edf1717a0dffd02f41b4be6254ceca6f
AryaBhole <bholeaarya@gmail.com>
2026-04-01 00:26:11 +0530

    MenuBars in swing along with mneunomics

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,5 +1,5 @@
 public class Main{ 
     public static void main(String[] args) { 
-    	ProgressBarDemo demo = new ProgressBarDemo();
+    	new MyFrame();
     } 
 } 
--- GUI/MyFrame.java
+++ GUI/MyFrame.java
@@ -1,33 +1,75 @@
-// Instead of using JFrame directly in the file you can use a parent class as such
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class MyFrame extends JFrame implements ActionListener{
-    //Make a constructor with the same code from the file but this saves time reusability and changes can be made with the original file itself
-    
+	JMenuBar menuBar;
+	JMenu fileMenu;
+	JMenu editMenu;
+	JMenu helpMenu;
+	JMenuItem loadItem;
+	JMenuItem saveItem;
+	JMenuItem exitItem;
+	//ImageIcon loadIcon;
+	//ImageIcon saveIcon;
+	//ImageIcon exitIcon;
 
-    MyFrame() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.setLayout(new FlowLayout());
+	MyFrame(){		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(100,100);
+		this.setLayout(new FlowLayout());
 
-        JCheckBox checkBox = new JCheckBox();    
-        checkBox.setText("I'm not a robot"); //or pass this directly
-        // can do same stuff as we have been doing uptill now with action listener and other font and icon stuff
+		//loadIcon = new ImageIcon("load.png");
+		//saveIcon = new ImageIcon("save.png");
+		//exitIcon = new ImageIcon("exit.png");
 
-        this.add(checkBox);
-        this.pack();
-        this.setVisible(true);
-    }
+		menuBar = new JMenuBar();
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        
-    }
-}   
+		fileMenu = new JMenu("File");
+		editMenu = new JMenu("Edit");
+		helpMenu = new JMenu("Help");
 
-// we can remove the actionListener tihing entirely and do button.addActionListener(e -> System.out.println());
-// this is a lambda expression
+		loadItem = new JMenuItem("Load");
+		saveItem = new JMenuItem("Save");
+		exitItem = new JMenuItem("Exit");
+
+		loadItem.addActionListener(this);
+		saveItem.addActionListener(this);
+		exitItem.addActionListener(this);
+
+		//loadItem.setIcon(loadIcon);
+		//saveItem.setIcon(saveIcon);
+		//exitItem.setIcon(exitIcon);
+
+		fileMenu.setMnemonic(KeyEvent.VK_F); // Alt + f for file
+		editMenu.setMnemonic(KeyEvent.VK_E); // Alt + e for edit
+		helpMenu.setMnemonic(KeyEvent.VK_H); // Alt + h for help
+		loadItem.setMnemonic(KeyEvent.VK_L); // l for load
+		saveItem.setMnemonic(KeyEvent.VK_S); // s for save
+		exitItem.setMnemonic(KeyEvent.VK_E); // e for exit
+
+		fileMenu.add(loadItem);
+		fileMenu.add(saveItem);
+		fileMenu.add(exitItem);
+
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+
+		this.setJMenuBar(menuBar);
+		this.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==loadItem) {
+			System.out.println("*beep boop* you loaded a file");
+		}
+		if(e.getSource()==saveItem) {
+			System.out.println("*beep boop* you saved a file");
+		}
+		if(e.getSource()==exitItem) {
+			System.exit(0);
+		}
+	}
+}
