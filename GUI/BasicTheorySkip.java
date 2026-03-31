53a36f6d44be904d72ba69d74ec6d3c9ee59bc72
AryaBhole <bholeaarya@gmail.com>
2026-03-31 05:55:33 +0530

    Theory skip part

---
+++ GUI/BasicTheorySkip.java
@@ -0,0 +1,5 @@
+//RadioButtons which can be used with buttonGroup to limit 1 choice and actionlistener to detect individual buttons within them
+
+//ComboBox is the drop down menu in which you can pass an array of refrence data type then the same actionListener stuff applies ther as well you can also add an item later
+
+//2 : 30 we continue normally with sliders in normal example based learning format
